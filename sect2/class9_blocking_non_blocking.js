e7bb4aa7ef3407dfb4b7dc824e5cbc24c9e54083
Alejandro <alejobecerra1995@gmail.com>
2021-01-11 22:16:55 -0500

    finished class notes about blocking and non blocking

--- sect2/class9_blocking_non_blocking.js
+++ sect2/class9_blocking_non_blocking.js
@@ -6,3 +6,9 @@
 // blocking is an idea invented to make writing software easier, but it is not really how a computer works.
 
 // hardware is async. The programming language is the one that makes the program execution blocking and sync
+
+// async code allows us to unlock the js engine and thus allow to execute other processes while waiting other processes to return
+// the different results of their operations. 
+
+// when an application is blocked, the cpu executes a context switch (which is saving the locked program into disk)
+// we need to try to avoid to be context switch. As when we are context switched, the cpu starts running other applications while our application is blocked.
